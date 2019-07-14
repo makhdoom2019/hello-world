88eea46da055c06e3a5f13850fbfff566c920af5
makhdoom2019 <49882510+makhdoom2019@users.noreply.github.com>
2019-07-14 14:22:48 +0430

    initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string movie_name ;
+    double adultTicketPrice,childTicketPrice,precentDonation,grossAmount,amountDonated,pureSaleAmount;
+    int noAdultTicketSold,noOFChildTicketSold;
+    cout <<"inter the name of movie"<<endl;
+    getline(cin,movie_name);
+    cout<<"movie_name:"<<".................."<<movie_name<<endl;
+
+
+
+
+    return 0;
+}
